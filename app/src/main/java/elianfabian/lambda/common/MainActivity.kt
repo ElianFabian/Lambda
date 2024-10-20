ElianFabian/ComputeIt

Commit for saving progress
I might split changes someday

app/src/main/java/elianfabian/computeit/common/MainActivity.kt
@@ -15,9 +15,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.lifecycleScope
 import com.zhuinden.simplestack.BackHandlingModel
+import com.zhuinden.simplestack.History
 import com.zhuinden.simplestack.SimpleStateChanger
 import com.zhuinden.simplestack.navigator.Navigator
+import com.zhuinden.simplestackextensions.lifecyclektx.observeAheadOfTimeWillHandleBackChanged
 import com.zhuinden.simplestackextensions.navigatorktx.backstack
 import com.zhuinden.simplestackextensions.services.DefaultServiceProvider
 import elianfabian.computeit.R
@@ -26,7 +29,10 @@ import elianfabian.computeit.common.util.callback.OnMainBackstackIsInitializedCa
 import elianfabian.computeit.common.util.simplestack.FragmentStateChanger
 import elianfabian.computeit.common.util.simplestack.ProcessDeathKeyFilter
 import elianfabian.computeit.common.util.simplestack.forEachServiceOfType
+import elianfabian.computeit.features.auth.presentation.login.LoginKey
 import elianfabian.computeit.ui.theme.ComputeItTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : FragmentActivity() {
 
@@ -52,6 +58,36 @@ class MainActivity : FragmentActivity() {
 		enableEdgeToEdge()
 
 		onBackPressedDispatcher.addCallback(backPressedCallback)
+		// TODO: make only production-release use the ComputeIt firebase project?
+		lifecycleScope.launch {
+//			val random = Random(42)
+//
+//			val time = measureTime {
+//				repeat(1_000_000) {
+//					random.nextInt()
+//				}
+//			}
+//
+//			println("$$$ time: $time")
+			//delay(1000)
+//			val message = "Spyro"
+//			val blob = Blob.fromBytes(message.toByteArray())
+//			Firebase.firestore.document("BlobCollection/blob")
+//				.set(mapOf("blob" to blob))
+//				.await()
+//
+//			val response = Firebase.firestore.document("BlobCollection/blob")
+//				.get()
+//				.await()
+//			
+//			val blobFromServer: Blob? = response.data?.get("blob") as? Blob
+//			println("$$$ blob = $blobFromServer")
+//			println("$$$ blob bytes = ${blobFromServer?.toBytes()}")
+//			val bytesToString = String(blobFromServer?.toBytes() ?: byteArrayOf())
+//			println("$$$ message = $bytesToString")	
+
+			delay(500)
+		}
 
 		setContent {
 			ComputeItTheme {
@@ -77,20 +113,20 @@ class MainActivity : FragmentActivity() {
 			.setScopedServices(DefaultServiceProvider())
 			.setGlobalServices(GlobalServiceProvider(application))
 			.setKeyFilter(ProcessDeathKeyFilter())
-//			.install(
-//				this,
-//				fragmentContainerView,
-//				History.single(TODO("Set initial key")),
-//			)
-//
-//		backPressedCallback.isEnabled = backstack.willHandleAheadOfTimeBack()
-//		backstack.observeAheadOfTimeWillHandleBackChanged(this) { willHandleBack ->
-//			backPressedCallback.isEnabled = willHandleBack
-//		}
-//
-//		if (savedInstanceState == null) {
-//			initialSetup()
-//		}
+			.install(
+				this,
+				fragmentContainerView,
+				History.single(LoginKey),
+			)
+
+		backPressedCallback.isEnabled = backstack.willHandleAheadOfTimeBack()
+		backstack.observeAheadOfTimeWillHandleBackChanged(this) { willHandleBack ->
+			backPressedCallback.isEnabled = willHandleBack
+		}
+
+		if (savedInstanceState == null) {
+			initialSetup()
+		}
 	}
 
 	@Suppress("UNCHECKED_CAST")

app/src/main/java/elianfabian/computeit/features/auth/di/RegisterModule.kt
@@ -1,0 +1,11 @@
+package elianfabian.computeit.features.auth.di
+
+import com.zhuinden.simplestack.ServiceBinder
+import elianfabian.computeit.common.util.simplestack.ServiceModule
+
+data object RegisterModule : ServiceModule {
+
+	override fun bindModuleServices(serviceBinder: ServiceBinder) {
+
+	}
+}

app/src/main/java/elianfabian/computeit/features/auth/presentation/components/EmailAuthButton.kt
@@ -1,0 +1,32 @@
+package elianfabian.computeit.features.auth.presentation.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import elianfabian.computeit.R
+import elianfabian.computeit.common.util.simplestack.compose.BasePreview
+
+@Composable
+fun EmailPasswordAuthButton(
+	text: String,
+	onClick: () -> Unit,
+	modifier: Modifier = Modifier,
+) {
+	BaseAuthButton(
+		text = text,
+		onClick = onClick,
+		iconStartResId = R.drawable.ic_password,
+		modifier = modifier,
+	)
+}
+
+
+@PreviewLightDark
+@Composable
+private fun Preview() = BasePreview {
+	EmailPasswordAuthButton(
+		text = stringResource(R.string.SignUpWithPassword),
+		onClick = {},
+	)
+}

app/src/main/java/elianfabian/computeit/features/auth/presentation/components/GoogleAuthButton.kt
@@ -1,0 +1,33 @@
+package elianfabian.computeit.features.auth.presentation.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import elianfabian.computeit.R
+import elianfabian.computeit.common.util.simplestack.compose.BasePreview
+
+@Composable
+fun GoogleAuthButton(
+	text: String,
+	onClick: () -> Unit,
+	modifier: Modifier = Modifier,
+) {
+	BaseAuthButton(
+		text = text,
+		onClick = onClick,
+		iconStartResId = R.drawable.ic_google,
+		iconTint = Color.Unspecified,
+		modifier = modifier
+	)
+}
+
+@PreviewLightDark
+@Composable
+private fun Preview() = BasePreview {
+	GoogleAuthButton(
+		text = stringResource(R.string.SignUpWithGoogle),
+		onClick = {},
+	)
+}

app/src/main/java/elianfabian/computeit/features/auth/presentation/login/LoginAction.kt
@@ -1,0 +1,11 @@
+package elianfabian.computeit.features.auth.presentation.login
+
+sealed interface LoginAction {
+	data class EnterUsername(val username: String) : LoginAction
+	data class EnterPassword(val password: String) : LoginAction
+	data object ClickLogin : LoginAction
+	data object ClickDontHaveAnAccountYet : LoginAction
+	data object ClickSignInWithEmail : LoginAction
+	data object ClickSignInWithGoogle : LoginAction
+	data object ClickContinueAsAGuest : LoginAction
+}

app/src/main/java/elianfabian/computeit/features/auth/presentation/login/LoginScreen.kt
@@ -1,0 +1,85 @@
+package elianfabian.computeit.features.auth.presentation.login
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import elianfabian.computeit.R
+import elianfabian.computeit.common.util.simplestack.compose.BasePreview
+import elianfabian.computeit.features.auth.presentation.components.ContinueAsAGuestButton
+import elianfabian.computeit.features.auth.presentation.components.EmailLinkAuthButton
+import elianfabian.computeit.features.auth.presentation.components.EmailPasswordAuthButton
+import elianfabian.computeit.features.auth.presentation.components.GoogleAuthButton
+
+@Composable
+fun LoginScreen(
+	onAction: (action: LoginAction) -> Unit,
+) {
+	Column(
+		verticalArrangement = Arrangement.Center,
+		horizontalAlignment = Alignment.CenterHorizontally,
+		modifier = Modifier
+			.fillMaxSize()
+			.padding(16.dp)
+			.padding(
+				bottom = WindowInsets.safeDrawing
+					.asPaddingValues()
+					.calculateBottomPadding()
+			)
+			.verticalScroll(rememberScrollState())
+	) {
+		Column(
+			verticalArrangement = Arrangement.spacedBy(8.dp),
+			horizontalAlignment = Alignment.CenterHorizontally,
+			modifier = Modifier
+				.fillMaxSize()
+		) {
+			EmailLinkAuthButton(
+				text = stringResource(R.string.SignInWithLink),
+				onClick = {
+					onAction(LoginAction.ClickSignInWithEmail)
+				},
+			)
+			EmailPasswordAuthButton(
+				text = stringResource(R.string.SignInWithPassword),
+				onClick = {
+					onAction(LoginAction.ClickSignInWithEmail)
+				},
+			)
+			GoogleAuthButton(
+				text = stringResource(R.string.SignInWithGoogle),
+				onClick = {
+					onAction(LoginAction.ClickSignInWithGoogle)
+				},
+			)
+			ContinueAsAGuestButton(
+				onClick = {
+					onAction(LoginAction.ClickContinueAsAGuest)
+				},
+				modifier = Modifier
+					.fillMaxWidth()
+			)
+		}
+	}
+}
+
+
+@PreviewLightDark
+@Composable
+private fun Preview() = BasePreview {
+	LoginScreen(
+		onAction = {},
+	)
+}

app/src/main/java/elianfabian/computeit/features/auth/presentation/register/RegisterScreen.kt
@@ -1,0 +1,11 @@
+package elianfabian.computeit.features.auth.presentation.register
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RegisterScreen(
+	modifier: Modifier = Modifier,
+) {
+
+}

ElianFabian/Lambda

app/src/main/java/elianfabian/lambda/common/MainActivity.kt
@@ -15,11 +15,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.lifecycleScope
 import com.zhuinden.simplestack.BackHandlingModel
+import com.zhuinden.simplestack.History
 import com.zhuinden.simplestack.SimpleStateChanger
 import com.zhuinden.simplestack.navigator.Navigator
+import com.zhuinden.simplestackextensions.lifecyclektx.observeAheadOfTimeWillHandleBackChanged
 import com.zhuinden.simplestackextensions.navigatorktx.backstack
 import com.zhuinden.simplestackextensions.services.DefaultServiceProvider
+import elianfabian.lambda.features.auth.presentation.login.LoginKey
 import elianfabian.lambda.R
 import elianfabian.lambda.common.di.GlobalServiceProvider
 import elianfabian.lambda.common.util.callback.OnMainBackstackIsInitializedCallback
@@ -27,6 +31,8 @@ import elianfabian.lambda.common.util.simplestack.FragmentStateChanger
 import elianfabian.lambda.common.util.simplestack.ProcessDeathKeyFilter
 import elianfabian.lambda.common.util.simplestack.forEachServiceOfType
 import elianfabian.lambda.ui.theme.LambdaThee
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : FragmentActivity() {
 
@@ -52,6 +58,36 @@ class MainActivity : FragmentActivity() {
 		enableEdgeToEdge()
 
 		onBackPressedDispatcher.addCallback(backPressedCallback)
+		// TODO: make only production-release use the ComputeIt firebase project?
+		lifecycleScope.launch {
+//			val random = Random(42)
+//
+//			val time = measureTime {
+//				repeat(1_000_000) {
+//					random.nextInt()
+//				}
+//			}
+//
+//			println("$$$ time: $time")
+			//delay(1000)
+//			val message = "Spyro"
+//			val blob = Blob.fromBytes(message.toByteArray())
+//			Firebase.firestore.document("BlobCollection/blob")
+//				.set(mapOf("blob" to blob))
+//				.await()
+//
+//			val response = Firebase.firestore.document("BlobCollection/blob")
+//				.get()
+//				.await()
+//			
+//			val blobFromServer: Blob? = response.data?.get("blob") as? Blob
+//			println("$$$ blob = $blobFromServer")
+//			println("$$$ blob bytes = ${blobFromServer?.toBytes()}")
+//			val bytesToString = String(blobFromServer?.toBytes() ?: byteArrayOf())
+//			println("$$$ message = $bytesToString")	
+
+			delay(500)
+		}
 
 		setContent {
 			LambdaThee {
@@ -77,20 +113,20 @@ class MainActivity : FragmentActivity() {
 			.setScopedServices(DefaultServiceProvider())
 			.setGlobalServices(GlobalServiceProvider(application))
 			.setKeyFilter(ProcessDeathKeyFilter())
-//			.install(
-//				this,
-//				fragmentContainerView,
-//				History.single(TODO("Set initial key")),
-//			)
-//
-//		backPressedCallback.isEnabled = backstack.willHandleAheadOfTimeBack()
-//		backstack.observeAheadOfTimeWillHandleBackChanged(this) { willHandleBack ->
-//			backPressedCallback.isEnabled = willHandleBack
-//		}
-//
-//		if (savedInstanceState == null) {
-//			initialSetup()
-//		}
+			.install(
+				this,
+				fragmentContainerView,
+				History.single(LoginKey),
+			)
+
+		backPressedCallback.isEnabled = backstack.willHandleAheadOfTimeBack()
+		backstack.observeAheadOfTimeWillHandleBackChanged(this) { willHandleBack ->
+			backPressedCallback.isEnabled = willHandleBack
+		}
+
+		if (savedInstanceState == null) {
+			initialSetup()
+		}
 	}
 
 	@Suppress("UNCHECKED_CAST")

app/src/main/java/elianfabian/lambda/features/auth/di/RegisterModule.kt
@@ -1,0 +1,11 @@
+package elianfabian.lambda.features.auth.di
+
+import com.zhuinden.simplestack.ServiceBinder
+import elianfabian.lambda.common.util.simplestack.ServiceModule
+
+data object RegisterModule : ServiceModule {
+
+	override fun bindModuleServices(serviceBinder: ServiceBinder) {
+
+	}
+}

app/src/main/java/elianfabian/lambda/features/auth/presentation/components/EmailAuthButton.kt
@@ -1,0 +1,32 @@
+package elianfabian.lambda.features.auth.presentation.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import elianfabian.lambda.R
+import elianfabian.lambda.common.util.simplestack.compose.BasePreview
+
+@Composable
+fun EmailPasswordAuthButton(
+	text: String,
+	onClick: () -> Unit,
+	modifier: Modifier = Modifier,
+) {
+	BaseAuthButton(
+		text = text,
+		onClick = onClick,
+		iconStartResId = R.drawable.ic_password,
+		modifier = modifier,
+	)
+}
+
+
+@PreviewLightDark
+@Composable
+private fun Preview() = BasePreview {
+	EmailPasswordAuthButton(
+		text = stringResource(R.string.SignUpWithPassword),
+		onClick = {},
+	)
+}

app/src/main/java/elianfabian/lambda/features/auth/presentation/components/GoogleAuthButton.kt
@@ -1,0 +1,33 @@
+package elianfabian.lambda.features.auth.presentation.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import elianfabian.lambda.R
+import elianfabian.lambda.common.util.simplestack.compose.BasePreview
+
+@Composable
+fun GoogleAuthButton(
+	text: String,
+	onClick: () -> Unit,
+	modifier: Modifier = Modifier,
+) {
+	BaseAuthButton(
+		text = text,
+		onClick = onClick,
+		iconStartResId = R.drawable.ic_google,
+		iconTint = Color.Unspecified,
+		modifier = modifier
+	)
+}
+
+@PreviewLightDark
+@Composable
+private fun Preview() = BasePreview {
+	GoogleAuthButton(
+		text = stringResource(R.string.SignUpWithGoogle),
+		onClick = {},
+	)
+}

app/src/main/java/elianfabian/lambda/features/auth/presentation/login/LoginAction.kt
@@ -1,0 +1,11 @@
+package elianfabian.lambda.features.auth.presentation.login
+
+sealed interface LoginAction {
+	data class EnterUsername(val username: String) : LoginAction
+	data class EnterPassword(val password: String) : LoginAction
+	data object ClickLogin : LoginAction
+	data object ClickDontHaveAnAccountYet : LoginAction
+	data object ClickSignInWithEmail : LoginAction
+	data object ClickSignInWithGoogle : LoginAction
+	data object ClickContinueAsAGuest : LoginAction
+}

app/src/main/java/elianfabian/lambda/features/auth/presentation/login/LoginScreen.kt
@@ -1,0 +1,85 @@
+package elianfabian.lambda.features.auth.presentation.login
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.dp
+import elianfabian.lambda.R
+import elianfabian.lambda.common.util.simplestack.compose.BasePreview
+import elianfabian.lambda.features.auth.presentation.components.ContinueAsAGuestButton
+import elianfabian.lambda.features.auth.presentation.components.EmailLinkAuthButton
+import elianfabian.lambda.features.auth.presentation.components.EmailPasswordAuthButton
+import elianfabian.lambda.features.auth.presentation.components.GoogleAuthButton
+
+@Composable
+fun LoginScreen(
+	onAction: (action: LoginAction) -> Unit,
+) {
+	Column(
+		verticalArrangement = Arrangement.Center,
+		horizontalAlignment = Alignment.CenterHorizontally,
+		modifier = Modifier
+			.fillMaxSize()
+			.padding(16.dp)
+			.padding(
+				bottom = WindowInsets.safeDrawing
+					.asPaddingValues()
+					.calculateBottomPadding()
+			)
+			.verticalScroll(rememberScrollState())
+	) {
+		Column(
+			verticalArrangement = Arrangement.spacedBy(8.dp),
+			horizontalAlignment = Alignment.CenterHorizontally,
+			modifier = Modifier
+				.fillMaxSize()
+		) {
+			EmailLinkAuthButton(
+				text = stringResource(R.string.SignInWithLink),
+				onClick = {
+					onAction(LoginAction.ClickSignInWithEmail)
+				},
+			)
+			EmailPasswordAuthButton(
+				text = stringResource(R.string.SignInWithPassword),
+				onClick = {
+					onAction(LoginAction.ClickSignInWithEmail)
+				},
+			)
+			GoogleAuthButton(
+				text = stringResource(R.string.SignInWithGoogle),
+				onClick = {
+					onAction(LoginAction.ClickSignInWithGoogle)
+				},
+			)
+			ContinueAsAGuestButton(
+				onClick = {
+					onAction(LoginAction.ClickContinueAsAGuest)
+				},
+				modifier = Modifier
+					.fillMaxWidth()
+			)
+		}
+	}
+}
+
+
+@PreviewLightDark
+@Composable
+private fun Preview() = BasePreview {
+	LoginScreen(
+		onAction = {},
+	)
+}

app/src/main/java/elianfabian/lambda/features/auth/presentation/register/RegisterScreen.kt
@@ -1,0 +1,11 @@
+package elianfabian.lambda.features.auth.presentation.register
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RegisterScreen(
+	modifier: Modifier = Modifier,
+) {
+
+}